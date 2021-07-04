d447eea8160f0a152504afba5d5f2c6682bcfb47
Mahmoud Elshaffie <mahmoud.elshaffie@gmail.com>
2021-07-04 19:51:31 +0200

    Maven SureFire plugin is added to fix tests initialization issue

---
+++ src/main/java/org/newstore/parsing/exceptions/InvalidPositionInputException.java
@@ -0,0 +1,19 @@
+package org.newstore.parsing.exceptions;
+
+public class InvalidPositionInputException extends IllegalArgumentException {
+
+	private static final long serialVersionUID = -7439430492026974834L;
+
+	public InvalidPositionInputException(String invalidInput, String expectedFormat) {
+		super(getFormattedMessage(invalidInput, expectedFormat));
+	}
+
+	public InvalidPositionInputException(String invalidInput, String expectedFormat, Exception cause) {
+		super(getFormattedMessage(invalidInput, expectedFormat), cause);
+	}
+
+	private static String getFormattedMessage(String invalidInput, String expectedFormat) {
+		String message = "Invalid Position: [%1$s] it doesn't match [%2$s]";
+		return String.format(message, invalidInput, expectedFormat);
+	}
+}
