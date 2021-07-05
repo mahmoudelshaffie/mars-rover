a917152cd8137579357e7583747a290314233234
Mahmoud Elshaffie <mahmoud.elshaffie@gmail.com>
2021-07-05 20:32:28 +0200

    Red test for move forward towards EAST is added

--- src/main/java/org/newstore/rover/Direction.java
+++ src/main/java/org/newstore/rover/Direction.java
@@ -1,5 +1,5 @@
 package org.newstore.rover;
 
 public enum Direction {
-	NORTH, SOUTH;
+	NORTH, SOUTH, EAST;
 }
--- src/test/java/org/newstore/rover/NewStoreRoverTest.java
+++ src/test/java/org/newstore/rover/NewStoreRoverTest.java
@@ -49,7 +49,7 @@ public class NewStoreRoverTest {
 	}
 	
 	@Test
-	public void testMoveGivenOnlyForwardToTheSouthFromInitialPositionShouldLongtitudeIncreasedByOneSuccessfully() {
+	public void testMoveGivenOnlyForwardToTheSouthFromInitialPositionShouldLongtitudeDecreasedByOneSuccessfully() {
 		// Given
 		int latitude = 0;
 		int longtitude = 0;
@@ -67,6 +67,27 @@ public class NewStoreRoverTest {
 		Direction expectDirectionNotChanged = Direction.SOUTH;
 		
 		assertRoverNewPosition(expectLatitudeNotChanged, expectLongtitudedDecreasedByOne, expectDirectionNotChanged, actual);
+	}
+	
+	@Test
+	public void testMoveGivenOnlyForwardToEastFromInitialPositionShouldLatitudeIncreasedByOneSuccessfully() {
+		// Given
+		int latitude = 0;
+		int longtitude = 0;
+		Position initialPosition = new Position(latitude, longtitude, Direction.EAST);
+		
+		NewStoreMarsRover marsRover = new NewStoreMarsRover(initialPosition);
+		String command = "F";
+		
+		// Actual
+		Position actual = marsRover.move(command);
+		
+		// Expected
+		int expectLatitudeIncreaseByOne = 1;
+		int expectLongtitudedNotChanged = 0;
+		Direction expectDirectionNotChanged = Direction.EAST;
+		
+		assertRoverNewPosition(expectLatitudeIncreaseByOne, expectLongtitudedNotChanged, expectDirectionNotChanged, actual);
 	}
 	
 	private void assertRoverNewPosition(int expectedLatitude, int expectedLongtitude, Direction expectedDirection, Position actual) {
