9f9d661d571e607a34da64b205a31f6886271ac5
Mahmoud Elshaffie <mahmoud.elshaffie@gmail.com>
2021-07-05 20:46:10 +0200

    Add move forward delta for coordinates for each direction

--- src/main/java/org/newstore/rover/Direction.java
+++ src/main/java/org/newstore/rover/Direction.java
@@ -1,5 +1,21 @@
 package org.newstore.rover;
 
 public enum Direction {
-	NORTH, SOUTH, EAST, WEST;
+	NORTH(0, 1), SOUTH(0, -1), EAST(1, 0), WEST(-1, 0);
+	
+	private int forwardLongtitudeDelta;
+	private int forwardLatitudeDelta;
+	
+	private Direction(int forwardLongtitudeDelta, int forwardLatitudeDelta) {
+		this.forwardLongtitudeDelta = forwardLongtitudeDelta;
+		this.forwardLatitudeDelta = forwardLatitudeDelta;
+	}
+	
+	public int getForwardLatitudeDelta() {
+		return forwardLatitudeDelta;
+	}
+	
+	public int getForwardLongtitudeDelta() {
+		return forwardLongtitudeDelta;
+	}
 }
