12c6a3fe1cdae0d2a12f9fb4a85daad5029200d5
Mahmoud Elshaffie <mahmoud.elshaffie@gmail.com>
2021-07-06 20:32:02 +0200

    Rotate Right from North Is Handled

--- src/main/java/org/newstore/rover/commands/RotateToRight.java
+++ src/main/java/org/newstore/rover/commands/RotateToRight.java
@@ -1,19 +1,30 @@
 package org.newstore.rover.commands;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.newstore.rover.Direction;
 import org.newstore.rover.Position;
 
 public class RotateToRight implements Command {
 
 	private Character commandInput = 'R';
+	private Map<Direction, Direction> rightDirections;
+	
+	public RotateToRight() {
+		rightDirections = new HashMap<>();
+		rightDirections.put(Direction.NORTH, Direction.EAST);
+	}
 
 	@Override
 	public Position move(Position current) {
-		throw new UnsupportedOperationException();
+		Direction currentDirection = current.getDirection();
+		Direction newDirection = rightDirections.get(currentDirection);
+		return current.newDirection(newDirection);
 	}
 
 	@Override
 	public Character getCommandInput() {
 		return commandInput;
 	}
-
 }
