b8882fbb991753dd4c5db99e28041332336f5d5a
Mahmoud Elshaffie <mahmoud.elshaffie@gmail.com>
2021-07-06 23:10:07 +0200

    Obstacle Parser Is Done Done

--- src/main/java/org/newstore/parsing/ObstaclesParser.java
+++ src/main/java/org/newstore/parsing/ObstaclesParser.java
@@ -1,12 +1,29 @@
 package org.newstore.parsing;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-public class ObstaclesParser implements InputParser<Map<Integer, Integer>>{
+import org.newstore.parsing.exceptions.InvalidObstacleInputException;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ObstaclesParser implements InputParser<Map<Integer, Integer>> {
+	
+	private final int LATITUDE = 0;
+	private final int LONGTITUDE = 1;
+
+	private TypeReference<List<Integer[]>> obstaclesTypeRef = new TypeReference<>(){};
+	
 	@Override
 	public Map<Integer, Integer> parse(String input) {
-		throw new UnsupportedOperationException();
+		try {
+			List<Integer[]> obstacles = new ObjectMapper().readValue(input, obstaclesTypeRef);
+			return obstacles.stream().collect(Collectors.toMap(obs -> obs[LATITUDE], obs -> obs[LONGTITUDE]));
+		} catch (Exception cause) {
+			throw new InvalidObstacleInputException(input, cause);
+		}
 	}
 
 }
--- src/test/java/org/newstore/parsing/ObstaclesParserTest.java
+++ src/test/java/org/newstore/parsing/ObstaclesParserTest.java
@@ -1,10 +1,12 @@
 package org.newstore.parsing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
+import org.newstore.parsing.exceptions.InvalidObstacleInputException;
 
 public class ObstaclesParserTest {
 
@@ -16,5 +18,11 @@ public class ObstaclesParserTest {
 		Map<Integer, Integer> actual = target.parse(validObstacles);
 		int expectedNoOfObstacels = 3;
 		assertEquals(expectedNoOfObstacels, actual.size());
+	}
+	
+	@Test
+	public void testParseInvalidValidObstaclesExpectFailWithInvalidObstaclesInputException() {
+		String validObstacles = "1, 4], [2, 3, [5, 7]]";
+		assertThrows(InvalidObstacleInputException.class, () -> target.parse(validObstacles));
 	}
 }
