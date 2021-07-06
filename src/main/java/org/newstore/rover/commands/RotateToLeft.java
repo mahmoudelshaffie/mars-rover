fd48674f9f11f702020ac49be09bb501ecf4dbe4
Mahmoud Elshaffie <mahmoud.elshaffie@gmail.com>
2021-07-06 20:40:18 +0200

    RotateLeft is refactored to remove switch

--- src/main/java/org/newstore/rover/commands/RotateToLeft.java
+++ src/main/java/org/newstore/rover/commands/RotateToLeft.java
@@ -1,39 +1,28 @@
 package org.newstore.rover.commands;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.newstore.rover.Direction;
 import org.newstore.rover.Position;
 
 public class RotateToLeft implements Command {
 
 	private Character commandInput = 'L';
-
+	private Map<Direction, Direction> leftDirections;
+	
+	public RotateToLeft() {
+		leftDirections = new HashMap<>();
+		leftDirections.put(Direction.NORTH, Direction.WEST);
+		leftDirections.put(Direction.WEST, Direction.SOUTH);
+		leftDirections.put(Direction.SOUTH, Direction.EAST);
+		leftDirections.put(Direction.EAST, Direction.NORTH);
+	}
+	
 	@Override
 	public Position move(Position current) {
 		Direction currentDirection = current.getDirection();
-		Direction newDirection;
-		
-		switch (currentDirection) {
-		case NORTH:
-			newDirection = Direction.WEST;
-			break;
-			
-		case WEST:
-			newDirection = Direction.SOUTH;
-			break;
-
-		case SOUTH:
-			newDirection = Direction.EAST;
-			break;
-			
-		case EAST:
-			newDirection = Direction.NORTH;
-			break;
-			
-		default:
-			newDirection = currentDirection;
-			break;
-		}
-		
+		Direction newDirection = leftDirections.get(currentDirection);
 		return current.newDirection(newDirection);
 	}
 
